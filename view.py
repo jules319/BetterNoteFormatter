0c23fcebc3bab269f96b9efc4530f99f462a5d43
jules319 <jules31901@gmail.com>
2023-10-31 16:08:29 -0400

    view class enhanced and callbacks/commands stripped out

--- view.py
+++ view.py
@@ -1,56 +1,75 @@
-# view.py
-
 import tkinter as tk
+from tkinter import ttk
 from tkinterdnd2 import DND_FILES, TkinterDnD
-# Import other necessary modules and methods...
 
-class PDFView:
-    def __init__(self, controller, root):
-        self.controller = controller
-        self.root = root
-        self.setup_ui()
+class PDFProcessorView(TkinterDnD.Tk):
 
-    def setup_ui(self):
+    def __init__(self):
+        super().__init__()
+        self.title("PDF Processor")
+        self.geometry("500x750")  
+        self.minsize(500, 750)  
+        self._setup_style()
+        self._build_ui()
+
+    def _setup_style(self):
+        style = ttk.Style(self)
+        pass
+
+    def _build_ui(self):
+        # Main frame
+        main_frame = ttk.Frame(self)
+        main_frame.pack(padx=10, pady=10, expand=True, fill=tk.BOTH)
+
         # Drag and Drop Area
-        self.drop_frame = tk.Frame(self.root, width=400, height=200, bd=2, relief="ridge")
-        self.drop_frame.pack(pady=20)
+        self.drop_frame = ttk.Labelframe(main_frame, width=400, height=150)  # Removed the text
+        self.drop_frame.grid(row=0, column=0, pady=20, sticky="ew", columnspan=2)
         self.drop_frame.drop_target_register(DND_FILES)
-        self.drop_frame.dnd_bind('<<Drop>>', self.controller.on_drop)
-        self.drop_label = tk.Label(self.drop_frame, text="Drag & Drop PDFs here or Select a Folder")
-        self.drop_label.pack(pady=80)
+        self.drop_label = ttk.Label(self.drop_frame, text="Drag & Drop PDFs here or")
+        self.drop_label.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+        self.folder_btn = ttk.Button(self.drop_frame, text="Select Folder")
+        self.folder_btn.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
+
+        # Text above the Listbox file queue
+        queue_label = ttk.Label(main_frame, text="Files to be processed:")
+        queue_label.grid(row=1, column=0, sticky="w", padx=10)
 
         # Queue Listbox with Scrollbar
-        self.queue_frame = tk.Frame(self.root)
-        self.queue_frame.pack(pady=20)
-
-        self.scrollbar = tk.Scrollbar(self.queue_frame, orient="vertical")
+        self.queue_frame = ttk.Frame(main_frame)
+        self.queue_frame.grid(row=2, column=0, pady=20, sticky="nsew", columnspan=2)
+        self.scrollbar = ttk.Scrollbar(self.queue_frame, orient="vertical")
         self.queue_listbox = tk.Listbox(self.queue_frame, yscrollcommand=self.scrollbar.set, height=10, width=50)
         self.scrollbar.config(command=self.queue_listbox.yview)
+        self.queue_listbox.grid(row=0, column=0, sticky="ew")
+        self.scrollbar.grid(row=0, column=1, sticky="ns")
+        self.queue_frame.columnconfigure(0, weight=1)
 
-        self.queue_listbox.pack(side=tk.LEFT, fill=tk.BOTH)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        # Buttons Frame
+        btn_frame = ttk.Frame(main_frame)
+        btn_frame.grid(row=3, column=0, pady=20, sticky="ew", columnspan=2)
+        self.remove_btn = ttk.Button(btn_frame, text="Remove Selected")
+        self.remove_btn.pack(side=tk.LEFT, padx=10, pady=5)
+        self.clear_btn = ttk.Button(btn_frame, text="Clear Queue")
+        self.clear_btn.pack(side=tk.RIGHT, padx=10, pady=5)
 
-        # Process Button
-        self.process_btn = tk.Button(self.root, text="Process PDFs", command=self.controller.process_files)
-        self.process_btn.pack(pady=20)
+        # Output Frame
+        output_frame = ttk.Labelframe(main_frame, text= "Output Folder:")
+        output_frame.grid(row=4, column=0, pady=20, sticky="ew", columnspan=2)
+        self.output_entry = ttk.Entry(output_frame, width=40)
+        self.output_entry.grid(row=0, column=1, padx=10, sticky="ew")
+        self.output_btn = ttk.Button(output_frame, text="Browse")
+        self.output_btn.grid(row=0, column=2, padx=10)
 
-        # Folder Selection Button
-        self.folder_btn = tk.Button(self.root, text="Select Folder", command=self.controller.process_folder)
-        self.folder_btn.pack(pady=20)
+        # Process PDFs button
+        self.process_btn = ttk.Button(main_frame, text="Process PDFs")
+        self.process_btn.grid(row=5, column=0, columnspan=2, pady=10)
 
-        # Clear Queue Button
-        self.clear_btn = tk.Button(self.root, text="Clear Queue", command=self.controller.clear_queue)
-        self.clear_btn.pack(pady=20)
+        # Text above Progress bar
+        progress_label = ttk.Label(main_frame, text="Processing Files...")
+        progress_label.grid(row=6, column=0, columnspan=2, pady=10, sticky="w", padx=10)
 
-        # Status Label
-        self.status_label = tk.Label(self.root, text="")
-        self.status_label.pack(pady=20)
-
-    def update_queue(self, pdf_queue):
-        self.queue_listbox.delete(0, tk.END)  # Clear the listbox
-        for file in pdf_queue:
-            self.queue_listbox.insert(tk.END, file)
-
-    def set_status(self, text):
-        self.status_label.config(text=text)
-
+        # Progress Bar
+        self.progress = ttk.Progressbar(main_frame, orient="horizontal", length=300, mode="determinate")
+        self.progress.grid(row=7, column=0, columnspan=2, pady=20, padx=10, sticky="ew")
+        main_frame.columnconfigure(0, weight=1)
+        output_frame.columnconfigure(1, weight=3)
